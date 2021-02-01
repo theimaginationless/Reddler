e4c4b73c5fd8599c0ffcd2c8bca555263a819eb5
Dmitry Teplyakov <dimon2242@gmail.com>
2021-02-01 23:18:50 +0300

    Add some views to detail post viewcontroller

--- Reddler/PostDetailViewController.swift
+++ Reddler/PostDetailViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class PostDetailViewController: UIViewController {
     @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subredditLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var imageCollectionView: ImageCollectionView!
+    @IBOutlet weak var imageCollectionViewHeightConstraint: NSLayoutConstraint!
     var post: RedditPost!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = self.post.title
+        self.title = self.post?.title
         self.contentTextView.text = self.post.selftext
+        self.titleLabel.text = self.post.title
+        self.authorLabel.text = self.post.author
+        self.subredditLabel.text = "r/\(self.post.subreddit)"
+        self.scoreLabel.text = "\(self.post.score)"
+        if self.post.images.count == 0 {
+            self.imageCollectionViewHeightConstraint.constant = 0
+        }
     }
 }
